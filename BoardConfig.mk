79185d1684ea2cbb3e2caacdd13d9568f2d0968f
Yuan Tong <tongyuan200097@gmail.com>
2025-05-14 21:22:45 +0800

    pdx234: Add kernel tweaks

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,8 @@ DEVICE_PATH := device/sony/pdx234
 TARGET_SCREEN_DENSITY := 420
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/sony/pdx234.config
+TARGET_KERNEL_CONFIG += vendor/sony/pdx234.config \
+                        kernel_extra.config
 
 # Props
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
